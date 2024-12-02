691146cfe08c2891daa566e2b4a5c6f40585f45e
Ashlee81 <mee60407@gmail.com>
2024-12-02 11:06:17 +0800

    Add files via upload

---
+++ data-dynamic-slider-bn.js
@@ -0,0 +1,133 @@
+// 需要複製請連著"{},"整個區塊複製
+// 是價格的話請用price，會加上$符號，不是價格請用'text'，不會加上$符號
+// 區塊： 主打星
+var dataDynamicSliderBn = [
+  // 1
+  [
+    {
+      name: '保健',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202010/412744bf-3778-4100-b222-553615a860f6.jpg',
+      webLink: 'https://www.etmall.com.tw/p/241990',
+      mobileLink: 'https://m.etmall.com.tw/p/241990',
+      appLink: 'etmall://open?pgid=53&eid=241990',    
+      text: '保健',
+    },
+    {
+      productNumber: 3382334, 
+      name: '日本味王 30:6:6高濃度金盞花葉黃素晶亮膠囊30粒X5盒',
+      price: 2512,
+    },
+  ],
+  // 2
+  [
+    {
+      name: '餐廚',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202207/9a40800e-07cf-479a-9a4f-261ee7f90a8f.jpg',
+      webLink: 'https://media.etmall.com.tw/sp/26340/m/index.html',
+      mobileLink: 'https://media.etmall.com.tw/sp/26340/m/index.html',
+      appLink: 'etmall://open?pgid=44&tb=0&url=https%3A%2F%2Fmedia.etmall.com.tw%2Fsp%2F26340%2Fm%2Fapp.html&t=%E9%8D%8B%E5%AF%B6%E5%B9%B4%E7%B5%82%E5%A4%A7%E8%B3%9E%7CETMall%E6%9D%B1%E6%A3%AE%E8%B3%BC%E7%89%A9%E7%B6%B2',   
+      text: '餐廚',
+    },
+    {
+      productNumber: 3306445, 
+      name: '【CookPower 鍋寶】金鑽不沾雙鍋四件組(32深炒鍋+28煎鍋+32蓋+鏟)-兩色可選',
+      price: 990, 
+    },
+  ],
+  // 3
+  [
+    {
+      name: '清潔',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202207/9a40800e-07cf-479a-9a4f-261ee7f90a8f.jpg',
+      webLink: 'https://www.etmall.com.tw/gp/197077',
+      mobileLink: 'https://m.etmall.com.tw/gp/197077',
+      appLink: 'etmall://open?pgid=65&eid=197077',    
+      text: '清潔',
+    },
+    {
+      productNumber: 4755571, 
+      name: '舒酸定 長效抗敏牙膏-牙齦護理120gX6入+贈大耳狗保鮮瀝水盒2個',
+      price: 599, 
+    },
+  ],
+  // 4
+  [
+    {
+      name: '3C',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202207/9a40800e-07cf-479a-9a4f-261ee7f90a8f.jpg',
+      webLink: 'https://media.etmall.com.tw/sp/13058/m/index.html',
+      mobileLink: 'https://media.etmall.com.tw/sp/13058/m/index.html',
+      appLink: 'etmall://open?pgid=44&tb=0&url=https%3A%2F%2Fmedia.etmall.com.tw%2Fsp%2F13058%2Fm%2Fapp.html&t=%E9%81%8A%E6%88%B2%E9%99%AA%E4%BD%A0%E5%97%A8%E7%BF%BB%E5%A4%A9%7CETMall%E6%9D%B1%E6%A3%AE%E8%B3%BC%E7%89%A9%E7%B6%B2',    
+      text: '3C',
+    },
+    {
+      productNumber: 7218367, 
+      name: 'SONY PS5 Marvels Spider-Man 2 蜘蛛人 2 限量版同捆組主機+副廠外出攜帶包【送PS炫光手持風扇】',
+      price: 20780, 
+    },
+  ],
+  // 5
+  [
+    {
+      name: '家電',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202207/9a40800e-07cf-479a-9a4f-261ee7f90a8f.jpg',
+      webLink: 'https://media.etmall.com.tw/sp/10591/m/index.html',
+      mobileLink: 'https://media.etmall.com.tw/sp/10591/m/index.html',
+      appLink: 'etmall://open?pgid=44&tb=0&url=https%3A%2F%2Fmedia.etmall.com.tw%2Fsp%2F10591%2Fm%2Fapp.html&t=LG%E5%AE%B6%E9%9B%BB%E9%9B%9911%E7%9B%9B%E6%83%A0%E2%98%85%E7%99%BB%E8%A8%98%E6%9C%80%E9%AB%98%E9%80%816%25%E7%84%A1%E4%B8%8A%E9%99%90%7CETMall%E6%9D%B1%E6%A3%AE%E8%B3%BC%E7%89%A9%E7%B6%B2',     
+      text: '家電',
+    },
+    {
+      productNumber: 4399419, 
+      name: 'LG樂金 15公斤 蒸氣滾筒洗衣機 (蒸洗脫烘) (冰磁白) WD-S15TBD',
+      price: 36999, 
+    },
+  ],
+  // 6
+  [
+    {
+      name: '保健',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202207/9a40800e-07cf-479a-9a4f-261ee7f90a8f.jpg',
+      webLink: 'https://www.etmall.com.tw/p/241990',
+      mobileLink: 'https://m.etmall.com.tw/p/241990',
+      appLink: 'etmall://open?pgid=53&eid=241990',    
+      text: '保健',
+    },
+    {
+      productNumber: 3382334, 
+      name: '日本味王 30:6:6高濃度金盞花葉黃素晶亮膠囊30粒X5盒',
+      price: 2512,
+    },
+  ],
+  // 7
+  [
+    {
+      name: '餐廚',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202207/9a40800e-07cf-479a-9a4f-261ee7f90a8f.jpg',
+      webLink: 'https://media.etmall.com.tw/sp/26340/m/index.html',
+      mobileLink: 'https://media.etmall.com.tw/sp/26340/m/index.html',
+      appLink: 'etmall://open?pgid=44&tb=0&url=https%3A%2F%2Fmedia.etmall.com.tw%2Fsp%2F26340%2Fm%2Fapp.html&t=%E9%8D%8B%E5%AF%B6%E5%B9%B4%E7%B5%82%E5%A4%A7%E8%B3%9E%7CETMall%E6%9D%B1%E6%A3%AE%E8%B3%BC%E7%89%A9%E7%B6%B2',   
+      text: '餐廚',
+    },
+    {
+      productNumber: 3306445, 
+      name: '【CookPower 鍋寶】金鑽不沾雙鍋四件組(32深炒鍋+28煎鍋+32蓋+鏟)-兩色可選',
+      price: 990, 
+    },
+  ],
+  // 8
+  [
+    {
+      name: '清潔',
+      imgSrc: 'https://media.etmall.com.tw/Promo/Image/Logo/202207/9a40800e-07cf-479a-9a4f-261ee7f90a8f.jpg',
+      webLink: 'https://www.etmall.com.tw/gp/197077',
+      mobileLink: 'https://m.etmall.com.tw/gp/197077',
+      appLink: 'etmall://open?pgid=65&eid=197077',    
+      text: '清潔',
+    },
+    {
+      productNumber: 4755571, 
+      name: '舒酸定 長效抗敏牙膏-牙齦護理120gX6入+贈大耳狗保鮮瀝水盒2個',
+      price: 599, 
+    },
+  ],
+];
